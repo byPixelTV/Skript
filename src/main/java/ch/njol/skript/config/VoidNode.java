f9bcc6cd289270cf259d58f88765f6d83a48e97f
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-11-16 20:12:06 +0100

    update VoidNode

--- src/main/java/ch/njol/skript/config/VoidNode.java
+++ src/main/java/ch/njol/skript/config/VoidNode.java
@@ -1,71 +1,29 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.config;
 
 /**
  * An empty line or a comment.
  * <p>
  * The subclass {@link InvalidNode} is used for invalid non-empty nodes, i.e. where a parsing error occurred.
- * 
- * @author Peter Güttinger
+ * </p>
  */
 public class VoidNode extends Node {
-	
-//	private final int initialLevel;
-//	private final String initialIndentation;
-	
-	VoidNode(final String line, final String comment, final SectionNode parent, final int lineNum) {
-		super("" + line.trim(), comment, parent, lineNum);
-//		initialLevel = getLevel();
-//		initialIndentation = "" + line.replaceFirst("\\S.*$", "");
+
+	VoidNode(String line, String comment, SectionNode parent, int lineNum) {
+		super(line.trim(), comment, parent, lineNum);
 	}
-	
-	@SuppressWarnings("null")
+
 	@Override
 	public String getKey() {
 		return key;
 	}
-	
-	public void set(final String s) {
+
+	public void set(String s) {
 		key = s;
 	}
-	
-	// doesn't work reliably
-//	@Override
-//	protected String getIndentation() {
-//		int levelDiff = getLevel() - initialLevel;
-//		if (levelDiff >= 0) {
-//			return StringUtils.multiply(config.getIndentation(), levelDiff) + initialIndentation;
-//		} else {
-//			final String ci = config.getIndentation();
-//			String ind = initialIndentation;
-//			while (levelDiff < 0 && ind.startsWith(ci)) {
-//				levelDiff++;
-//				ind = "" + ind.substring(ci.length());
-//			}
-//			return ind;
-//		}
-//	}
-	
+
 	@Override
 	String save_i() {
-		return "" + key;
+		return key;
 	}
-	
+
 }
