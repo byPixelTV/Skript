830e7588e6d150c8854c6e45a443deced1f75602
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-11-19 20:53:37 +0100

    update to make it actually work

--- src/main/java/ch/njol/skript/config/Config.java
+++ src/main/java/ch/njol/skript/config/Config.java
@@ -2,16 +2,19 @@ package ch.njol.skript.config;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.config.validate.SectionValidator;
-import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -44,11 +47,11 @@ public class Config implements Comparable<Config> {
 	final boolean allowEmptySections;
 
 	String fileName;
-	@Nullable
-	Path file = null;
+	@Nullable Path file = null;
 
-	public Config(final InputStream source, final String fileName, @Nullable final File file, final boolean simple, final boolean allowEmptySections, final String defaultSeparator) throws IOException {
-		try {
+	public Config(InputStream source, String fileName, @Nullable File file,
+				  boolean simple, boolean allowEmptySections, String defaultSeparator) throws IOException {
+		try (source) {
 			this.fileName = fileName;
 			if (file != null) // Must check for null before converting to path
 				this.file = file.toPath();
@@ -69,76 +72,79 @@ public class Config implements Comparable<Config> {
 			try (ConfigReader reader = new ConfigReader(source)) {
 				main = SectionNode.load(this, reader);
 			}
-		} finally {
-			source.close();
 		}
 	}
 
-	public Config(final InputStream source, final String fileName, final boolean simple, final boolean allowEmptySections, final String defaultSeparator) throws IOException {
+	public Config(InputStream source, String fileName, boolean simple,
+				  boolean allowEmptySections, String defaultSeparator) throws IOException {
 		this(source, fileName, null, simple, allowEmptySections, defaultSeparator);
 	}
 
-	public Config(final File file, final boolean simple, final boolean allowEmptySections, final String defaultSeparator) throws IOException {
+	public Config(File file, boolean simple,
+				  boolean allowEmptySections, String defaultSeparator) throws IOException {
 		this(Files.newInputStream(file.toPath()), file.getName(), simple, allowEmptySections, defaultSeparator);
 		this.file = file.toPath();
 	}
 
-	@SuppressWarnings("null")
-	public Config(final Path file, final boolean simple, final boolean allowEmptySections, final String defaultSeparator) throws IOException {
+	public Config(@NotNull Path file, boolean simple,
+				  boolean allowEmptySections, String defaultSeparator) throws IOException {
 		this(Channels.newInputStream(FileChannel.open(file)), "" + file.getFileName(), simple, allowEmptySections, defaultSeparator);
 		this.file = file;
 	}
 
 	/**
-	 * For testing
-	 *
-	 * @param s
-	 * @param fileName
-	 * @param simple
-	 * @param allowEmptySections
-	 * @param defaultSeparator
-	 * @throws IOException
+	 * Sets all {@link Option} fields of the given object to the values from this config
 	 */
-	public Config(final String s, final String fileName, final boolean simple, final boolean allowEmptySections, final String defaultSeparator) throws IOException {
-		this(new ByteArrayInputStream(s.getBytes(ConfigReader.UTF_8)), fileName, simple, allowEmptySections, defaultSeparator);
+	public void load(Object object) {
+		load(object.getClass(), object, "");
 	}
 
-	void setIndentation(final String indent) {
+	/**
+	 * Sets all static {@link Option} fields of the given class to the values from this config
+	 */
+	public void load(Class<?> clazz) {
+		load(clazz, null, "");
+	}
+
+	private void load(Class<?> clazz, @Nullable Object object, String path) {
+		for (Field field : clazz.getDeclaredFields()) {
+			field.setAccessible(true);
+			if (object != null || Modifier.isStatic(field.getModifiers())) {
+				try {
+					if (OptionSection.class.isAssignableFrom(field.getType())) {
+						OptionSection section = (OptionSection) field.get(object);
+						@NotNull Class<?> pc = section.getClass();
+						load(pc, section, path + section.key + ".");
+					} else if (Option.class.isAssignableFrom(field.getType())) {
+						((Option<?>) field.get(object)).set(this, path);
+					}
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					assert false;
+				}
+			}
+		}
+	}
+
+	void setIndentation(String indent) {
 		assert indent != null && !indent.isEmpty() : indent;
 		indentation = indent;
-		indentationName = (indent.charAt(0) == ' ' ? "space" : "tab");
-	}
-
-	String getIndentation() {
-		return indentation;
-	}
-
-	String getIndentationName() {
-		return indentationName;
-	}
-
-	public SectionNode getMainNode() {
-		return main;
-	}
-
-	public String getFileName() {
-		return fileName;
+		indentationName = indent.charAt(0) == ' ' ? "space" : "tab";
 	}
 
 	/**
 	 * Saves the config to a file.
 	 *
-	 * @param f The file to save to
+	 * @param file The file to save to
 	 * @throws IOException If the file could not be written to.
 	 */
-	public void save(final File f) throws IOException {
+	public void save(File file) throws IOException {
 		separator = defaultSeparator;
-		final PrintWriter w = new PrintWriter(f, "UTF-8");
+		PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8);
 		try {
-			main.save(w);
+			main.save(writer);
 		} finally {
-			w.flush();
-			w.close();
+			writer.flush();
+			writer.close();
 		}
 	}
 
@@ -162,6 +168,15 @@ public class Config implements Comparable<Config> {
 		return getMainNode().setValues(other.getMainNode(), excluded);
 	}
 
+	/*
+	PLAN:
+	1) get nodes that have changed between configs
+	2) get node differences
+	3) add nodes:
+		- if node at index is already occupied, replace it
+		- if node at index is empty, add it
+	 */
+
 	/**
 	 * Updates the keys of this config with the keys of another config.
 	 * Used for updating a config file to a newer version.
@@ -171,138 +186,94 @@ public class Config implements Comparable<Config> {
 	 * @return True if any keys were added to this config, false otherwise.
 	 */
 	public boolean updateKeys(@NotNull Config newer) {
-		Set<String> newKeys = findKeys(newer.getMainNode(), "");
-		Set<String> oldKeys = findKeys(getMainNode(), "");
+		Set<Node> newNodes = findNodes(newer.getMainNode());
+		Set<Node> oldNodes = findNodes(getMainNode());
 
-		newKeys.removeAll(oldKeys);
-		Set<String> missingKeys = new LinkedHashSet<>(newKeys);
+		System.out.println(newNodes);
+		System.out.println(oldNodes);
 
-		if (missingKeys.isEmpty())
+		newNodes.removeAll(oldNodes);
+		Set<Node> nodesToUpdate = new LinkedHashSet<>(newNodes);
+
+		System.out.println(nodesToUpdate);
+
+		if (nodesToUpdate.isEmpty())
 			return false;
 
-		for (String key : missingKeys) {
-			Node node = newer.getNode(key);
-			if (!(node instanceof EntryNode entryNode))
-				continue;
+		for (Node node : nodesToUpdate) {
+			int idx = node.getFullIndex();
+			String path = node.getPath();
+			SectionNode newParent = node.getParent();
+			if (newParent == null)
+				throw new IllegalStateException("newParent is null");
 
-			updateEntry(key, entryNode);
+			String pathToParent = newParent.getPath();
+
+			System.out.println(idx);
+			System.out.println("path: " + path);
+			System.out.println("pathToParent: " + pathToParent);
+
+			SectionNode parent = getNode(pathToParent);
+			System.out.println("parent: " + parent);
+			if (parent == null)
+				throw new IllegalStateException("parent is null");
+
+			if (idx >= parent.size()) { // add
+				System.out.println("adding due to oversize " + node);
+				parent.add(node);
+				continue;
+			}
+
+			Node existing = parent.getFull(idx);
+			if (existing != null) { // replace existing
+				System.out.println("replacing " + existing + " with " + node);
+				parent.remove(existing);
+				parent.add(idx, node);
+			} else { // add
+				System.out.println("adding " + node);
+				parent.add(node);
+			}
 		}
 		return true;
 	}
 
-	// TODO COPY VOIDNODES RELATIVE TO
-	private void updateEntry(@NotNull String key, @NotNull EntryNode node) {
-		int idx = node.getIndex();
-		int splitAt = key.lastIndexOf('.');
-		if (splitAt == -1) { // top level key
-			getMainNode().add(idx, new EntryNode(key, node.getValue(), node.getComment(), getMainNode(), idx));
-			return;
-		}
-
-		String pathToKey = key.substring(0, splitAt);
-		String leafKey = key.substring(splitAt + 1); // exclude .
-
-		Node parent = getNode(pathToKey);
-
-		if (parent == null) // parent section does not exist, so check all ancestors and add them if missing
-			parent = addMissingAncestors(pathToKey.split("\\.")).getLast();
-
-		if (parent instanceof SectionNode parentSection) {
-			parentSection.add(idx, new EntryNode(leafKey, node.getValue(), node.getComment(), parentSection, -1));
-		}
-	}
-
 	/**
-	 * Adds possibly missing ancestor nodes specified in {@code keys}.
-	 *
-	 * @param keys An array of keys to add as ancestors.
-	 * @return A list of the added nodes.
-	 */
-	private List<Node> addMissingAncestors(String[] keys) {
-		List<Node> added = new ArrayList<>();
-		SectionNode parent = getMainNode();
-
-		for (String key : keys) {
-			Node node = parent.get(key);
-			if (node == null) {
-				// TODO COMMENT SUPPORT
-				SectionNode newNode = new SectionNode(key, "", parent, -1);
-				parent.add(newNode);
-				added.add(newNode);
-			} else {
-				Preconditions.checkArgument(node instanceof SectionNode, "node is not a section node");
-				parent = (SectionNode) node;
-			}
-		}
-
-		return added;
-	}
-
-	/**
-	 * Recursively finds all keys in a section node.
-	 * <p>Keys are represented in dot notation, e.g. {@code grandparent.parent.child}.</p>
+	 * Recursively finds all nodes in a section node.
 	 *
 	 * @param node The parent node to search.
-	 * @param key  The built key of the current node.
-	 *             Should be empty when calling this method outside the method itself.
-	 * @return A set of the discovered keys, guaranteed to be in the order of discovery.
+	 * @return A set of the discovered nodes, guaranteed to be in the order of discovery.
 	 */
 	@Contract(pure = true)
-	private static Set<String> findKeys(@NotNull SectionNode node, @NotNull String key) {
-		Set<String> keys = new LinkedHashSet<>();
+	private static @NotNull Set<Node> findNodes(@NotNull SectionNode node) {
+		Set<Node> nodes = new LinkedHashSet<>();
 
-		if (!key.isEmpty()) {
-			key += ".";
-		}
-
-		for (Node child : node) {
+		for (Iterator<Node> iterator = node.fullIterator(); iterator.hasNext(); ) {
+			Node child = iterator.next();
 			if (child instanceof SectionNode sectionNode) {
-				keys.addAll(findKeys(sectionNode, key + sectionNode.getKey()));
-			} else if (child instanceof EntryNode entryNode) {
-				keys.add(key + entryNode.getKey());
+				nodes.add(child);
+				nodes.addAll(findNodes(sectionNode));
+			} else if (child instanceof EntryNode || child instanceof VoidNode) {
+				nodes.add(child);
 			}
 		}
-		return keys;
+		return nodes;
 	}
 
-	/**
-	 * Gets a node at the given path, split by dot characters. May be null.
-	 *
-	 * @param path The path to the section node.
-	 * @return The node at the given path or null if it does not exist.
-	 */
-	public @Nullable Node getNode(@NotNull String path) {
-		return getNode(main, new LinkedList<>(List.of(path.split("\\."))));
+	private SectionNode getNode(String path) {
+		return getNode(path.split("\\."));
 	}
 
-	/**
-	 * Gets a node at the given path. May be null.
-	 *
-	 * @param path The path to the section node.
-	 * @return The node at the given path or null if it does not exist.
-	 */
-	public @Nullable Node getNode(@NotNull String... path) {
-		return getNode(main, new LinkedList<>(List.of(path)));
-	}
+	private SectionNode getNode(String... path) {
+		SectionNode node = getMainNode();
+		for (String key : path) {
+			Node child = node.get(key);
 
-	/**
-	 * Recursively gets a node at the given path. May be null.
-	 *
-	 * @param section The current section node to search.
-	 * @param path    The remaining path to the desired node.
-	 * @return The node at the given path or null if it does not exist.
-	 * @see #getNode(String)
-	 * @see #getNode(String...)
-	 */
-	private @Nullable Node getNode(@NotNull SectionNode section, @NotNull Queue<String> path) {
-		String head = path.poll();
-		if (head == null)
-			return section;
-
-		Node node = section.get(head);
-		if (node instanceof SectionNode sectionNode)
-			return getNode(sectionNode, path);
-
+			if (child instanceof SectionNode sectionNode) {
+				node = sectionNode;
+			} else {
+				return node;
+			}
+		}
 		return node;
 	}
 
@@ -318,8 +289,61 @@ public class Config implements Comparable<Config> {
 		return getMainNode().compareValues(other.getMainNode(), excluded);
 	}
 
-	@Nullable
-	public File getFile() {
+	/**
+	 * Splits the given path at the dot character and passes the result to {@link #get(String...)}.
+	 *
+	 * @param path
+	 * @return <tt>get(path.split("\\."))</tt>
+	 */
+	public @Nullable String getByPath(@NotNull String path) {
+		return get(path.split("\\."));
+	}
+
+	/**
+	 * Gets an entry node's value at the designated path
+	 *
+	 * @param path
+	 * @return The entry node's value at the location defined by path or null if it either doesn't exist or is not an entry.
+	 */
+	public @Nullable String get(String... path) {
+		SectionNode section = main;
+		for (int i = 0; i < path.length; i++) {
+			Node node = section.get(path[i]);
+			if (node == null)
+				return null;
+			if (node instanceof SectionNode sectionNode) {
+				if (i == path.length - 1)
+					return null;
+				section = sectionNode;
+			} else {
+				if (node instanceof EntryNode entryNode && i == path.length - 1)
+					return entryNode.getValue();
+				else
+					return null;
+			}
+		}
+		return null;
+	}
+
+	public HashMap<String, String> toMap(final String separator) {
+		return main.toMap("", separator);
+	}
+
+	public boolean validate(final SectionValidator validator) {
+		return validator.validate(getMainNode());
+	}
+
+	/**
+	 * @return Whether the config is empty.
+	 */
+	public boolean isEmpty() {
+		return main.isEmpty();
+	}
+
+	/**
+	 * @return The file this config was loaded from, or null if it was loaded from an InputStream.
+	 */
+	public @Nullable File getFile() {
 		if (file != null) {
 			try {
 				return file.toFile();
@@ -330,8 +354,10 @@ public class Config implements Comparable<Config> {
 		return null;
 	}
 
-	@Nullable
-	public Path getPath() {
+	/**
+	 * @return The path this config was loaded from, or null if it was loaded from an InputStream.
+	 */
+	public @Nullable Path getPath() {
 		return file;
 	}
 
@@ -353,88 +379,20 @@ public class Config implements Comparable<Config> {
 		return " " + separator + " ";
 	}
 
-	/**
-	 * Splits the given path at the dot character and passes the result to {@link #get(String...)}.
-	 *
-	 * @param path
-	 * @return <tt>get(path.split("\\."))</tt>
-	 */
-	@SuppressWarnings("null")
-	@Nullable
-	public String getByPath(final String path) {
-		return get(path.split("\\."));
+	String getIndentation() {
+		return indentation;
 	}
 
-	/**
-	 * Gets an entry node's value at the designated path
-	 *
-	 * @param path
-	 * @return The entry node's value at the location defined by path or null if it either doesn't exist or is not an entry.
-	 */
-	@Nullable
-	public String get(final String... path) {
-		SectionNode section = main;
-		for (int i = 0; i < path.length; i++) {
-			final Node n = section.get(path[i]);
-			if (n == null)
-				return null;
-			if (n instanceof SectionNode) {
-				if (i == path.length - 1)
-					return null;
-				section = (SectionNode) n;
-			} else {
-				if (n instanceof EntryNode && i == path.length - 1)
-					return ((EntryNode) n).getValue();
-				else
-					return null;
-			}
-		}
-		return null;
+	String getIndentationName() {
+		return indentationName;
 	}
 
-	public boolean isEmpty() {
-		return main.isEmpty();
+	public SectionNode getMainNode() {
+		return main;
 	}
 
-	public HashMap<String, String> toMap(final String separator) {
-		return main.toMap("", separator);
-	}
-
-	public boolean validate(final SectionValidator validator) {
-		return validator.validate(getMainNode());
-	}
-
-	private void load(final Class<?> cls, final @Nullable Object object, final String path) {
-		for (final Field field : cls.getDeclaredFields()) {
-			field.setAccessible(true);
-			if (object != null || Modifier.isStatic(field.getModifiers())) {
-				try {
-					if (OptionSection.class.isAssignableFrom(field.getType())) {
-						final OptionSection section = (OptionSection) field.get(object);
-						@NotNull final Class<?> pc = section.getClass();
-						load(pc, section, path + section.key + ".");
-					} else if (Option.class.isAssignableFrom(field.getType())) {
-						((Option<?>) field.get(object)).set(this, path);
-					}
-				} catch (final IllegalArgumentException | IllegalAccessException e) {
-					assert false;
-				}
-			}
-		}
-	}
-
-	/**
-	 * Sets all {@link Option} fields of the given object to the values from this config
-	 */
-	public void load(final Object o) {
-		load(o.getClass(), o, "");
-	}
-
-	/**
-	 * Sets all static {@link Option} fields of the given class to the values from this config
-	 */
-	public void load(final Class<?> c) {
-		load(c, null, "");
+	public String getFileName() {
+		return fileName;
 	}
 
 	@Override
